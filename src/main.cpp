f0f8d21f025a3ae5a0d3abcc6b953d4901b0ad23
kopec47 <274410@student.pwr.edu.pl>
2026-04-27 16:44:04 +0200

    dodałem tryb battery saver (deep_sleep) i poprawiłem format wysylanych danych do aplikacji

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,22 @@
 #define SERVICE_UUID        "12345678-1234-1234-1234-123456789abc"
 #define CHARACTERISTIC_UUID   "abcdefab-1234-5678-1234-56789abcdef0"
 
+#define uS_TO_S_FACTOR 1000000ULL
+#define TIME_TO_SLEEP  1800
+#define BLE_TIMEOUT 60000
 
 Adafruit_BME280 bme; //czujnik
 BLECharacteristic *pCharacteristic;
 bool deviceConnected = false;
+unsigned long bootTime = 0;
+
+void goToDeepSleep(){
+  Serial.println("Przechodzenie do głębokiego snu...");
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  Serial.flush();
+  esp_deep_sleep_start();
+
+}
 
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
@@ -27,19 +39,32 @@ class MyServerCallbacks: public BLEServerCallbacks {
     void onDisconnect(BLEServer* pServer) {
       deviceConnected = false;
       Serial.println("Klient rozłączony!");
-      pServer-> getAdvertising()->start(); // Ponowne rozpoczęcie reklamowania po rozłączeniu klienta 
+      pServer-> getAdvertising()->start(); // Ponowne rozpoczęcie transmisji reklamowej po rozłączeniu klienta
+      delay(500);
+      goToDeepSleep();
+
     }
 };
 
 void setup(){
   Serial.begin(115200);
+  bootTime = millis();
+  
+  esp_sleep_wakeup_cause_t wakeupReason = esp_sleep_get_wakeup_cause();
+  if (wakeupReason == ESP_SLEEP_WAKEUP_TIMER) {
+    Serial.println("wybudzenie z głębokiego snu po upływie czasu '30 s'.");
+  } else {
+    Serial.println("Wybudzenie urzadzenia przez przycisk 'RESET'");
+  }
 
 
   Wire.begin(I2C_SDA, I2C_SCL); 
   if (!bme.begin(0x76, &Wire)) {
     Serial.println("Nie można znaleźć czujnika BME280!");
-    while (1);
+    delay(2000);
+    goToDeepSleep();
   }
+
   BLEDevice::init("ESP32 Weather Station");
   BLEServer *pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
@@ -67,13 +92,21 @@ void setup(){
 
 void loop(){
   
+
+  if(!deviceConnected && (millis() - bootTime > BLE_TIMEOUT)) {
+    Serial.println("Brak połączenia z klientem BLE w ciągu 60 sekund. Przechodzenie do głębokiego snu...");
+    goToDeepSleep();
+  } 
+
+
   float temperature = bme.readTemperature();
   float humidity = bme.readHumidity();
   float pressure = bme.readPressure() / 100.0F; // konwersja do hPa
+  
+  
 
-  String dataString = "Temperatura: " + String(temperature) + " °C, " +
-                      "Wilgotność: " + String(humidity) + " %, " +
-                      "Ciśnienie: " + String(pressure) + " hPa";
+  String dataString = String(temperature) + "," + String(humidity) + "," + String(pressure);
+
 
   if (deviceConnected) {
     pCharacteristic->setValue(dataString.c_str());
